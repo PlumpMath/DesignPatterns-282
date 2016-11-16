73a2107faec1f03a5dd0c12bc2d4598b5dffcf4b
Vince Pergolizzi <pergolizzivincenzo@gmail.com>
2016-11-15 22:46:15 -0500

    Added SingletonExample

---
+++ DesignPatterns.SingletonExample/FtpSettings.cs
@@ -0,0 +1,26 @@
+﻿namespace DesignPatterns.SingletonExample
+{
+    public sealed class FtpSettings
+    {
+        public static FtpSettings Instance { get { return instance; } }
+
+        private static readonly FtpSettings instance = new FtpSettings();
+
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        static FtpSettings()
+        {
+        }
+
+        private FtpSettings()
+        {
+            Host = "127.0.0.1";
+            Port = 21;
+            Username = "username";
+            Password = "password";
+        }
+    }
+}
---
+++ DesignPatterns.SingletonExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DesignPatterns.SingletonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Host: \t\t{0}", FtpSettings.Instance.Host);
+            Console.WriteLine("Port: \t\t{0}", FtpSettings.Instance.Port);
+            Console.WriteLine("Username: \t{0}", FtpSettings.Instance.Username);
+            Console.WriteLine("Password: \t{0}", FtpSettings.Instance.Password);
+
+            Console.ReadLine();
+        }
+    }
+}
